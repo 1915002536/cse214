4cf83869071406095cffa4bbf05455eb7d940473
1915002536 <71250081+1915002536@users.noreply.github.com>
2020-10-19 11:44:24 +0600

    Add files via upload

---
+++ Problem Set 1 - Array/4. Print entire array each element in newline.c
@@ -0,0 +1,18 @@
+/*
+Print entire array each element in newline.
+Name: Shofiqul Islam
+ID: 1915002536
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
