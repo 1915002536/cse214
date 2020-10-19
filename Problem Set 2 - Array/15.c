bdacd3c9e1c0503206d8fe545344ec444c5efaf1
1915002536 <71250081+1915002536@users.noreply.github.com>
2020-10-20 00:14:22 +0600

    Add files via upload

---
+++ Problem Set 2 - Array/15.c
@@ -0,0 +1,70 @@
+/*
+Delete a value X (take input X from user) from the array shift all other value to left.If X
+is not present in the given array print Element Not Found.Delete all the occurrence of the value.
+Name: Shofiqul Islam
+ID: 1915002536
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, j, x, k=-1;
+    int size = 10;
+
+    printf("The values of array : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    printf("Enter the value of X :\n");
+    scanf("%d",&x);
+
+    for(i=0; i<size; i++)
+    {
+        if(x == a[i])
+        {
+            k=i;
+        }
+    }
+    if(k == -1)
+    {
+        printf("\nElement Not Found\n");
+    }
+
+    else
+    {
+        printf("Before :\n");
+        for(i=0; i<size; i++)
+        {
+            printf("%d ",a[i]);
+        }
+
+        printf("\n");
+
+        for(i=0; i<size; i++)
+        {
+            if(x == a[i])
+            {
+                k=i;
+                for(j=k; j<size-1; j++)
+                {
+                    a[j] = a[j+1];
+                }
+                size--;
+            }
+        }
+
+        printf("After : \n");
+        for(i=0; i<size; i++)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+
+    return 0;
+}
+
+
+
